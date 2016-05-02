dc069eadffe7b9332f619fe18201f8df2a17a8f1
Luis Toubes <luis@toub.es>
2016-05-03 01:13:38 +0200

    Refactored language selection

--- src/client/app/app.run.js
+++ src/client/app/app.run.js
@@ -1,28 +1,30 @@
 (function() {
   'use strict';
-  angular.module('drexler')
-    .run(run);
+  angular.module('drexler').run(run);
 
-  function run($ionicPlatform,$cordovaGlobalization, drexlerStorage, gettextCatalog){
-
+  /* @ngInject */
+  function run($ionicPlatform, $cordovaGlobalization, drexlerStorage, gettextCatalog, $log, DRX_DEFAULT_LANGUAGE){
+    var log = $log.getInstance('drexler');
     // Coding with love :A
     $ionicPlatform.ready(function() {
-      // Applying logic for Language from Localstorage > Language from Device > Default Language :A
-      if (drexlerStorage.get('language')) { // Checking if local storage varibale for drexler exists :A
-        gettextCatalog.setCurrentLanguage(drexlerStorage.get('language').id);
-      }
-      else { // If the local variable for language doesn't exists check for device language :A
-        if ($ionicPlatform.is.name.length > 0) { // This is to just check for platform so that we don't get error on borwser :A
+      // Applying logic for Language from Localstorage > Language from Device > Default Language
+      var languageSelected = drexlerStorage.get('language') || DRX_DEFAULT_LANGUAGE;
+      if (angular.isObject(languageSelected)) { // Checking if local storage varibale for drexler exists
+        log.debug('%s language from localstorage', languageSelected.id);
+        languageSelected = languageSelected.id;
+      } else { // If the local variable for language doesn't exists check for device language
+        if ($ionicPlatform.is.name.length > 0) { // This is to just check for platform so that we don't get error on browser
           $cordovaGlobalization.getPreferredLanguage().then(
-            function (deviceLang) { // Setting device language :A
-              gettextCatalog.setCurrentLanguage(deviceLang.value);
+            function (deviceLang) { // Setting device language
+              log.debug('%s language from device', deviceLang.value);
+              languageSelected = deviceLang.value;
             },
-            function (error) { // If We can not get language from device either because shit happens sometimes we go for default :A
-              gettextCatalog.setCurrentLanguage('en-EN'); // We will move this drexler constants :A
+            function (error) { // If We can not get language from device either because shit happens sometimes we go for default
+              log.error('Can not detect language in device, selecting default', error);
             });
         }
       }
-      // EO - Language selection :A
+      gettextCatalog.setCurrentLanguage(languageSelected);
     });
 
   }
--- src/client/app/core/core.service.constants.js
+++ src/client/app/core/core.service.constants.js
@@ -4,5 +4,6 @@
     .constant('DRX_LANGUAGE',[
       {id: 'en-US', name: 'English'},
       {id: 'es-ES', name: 'Spanish'}
-    ]);
+    ])
+    .constant('DRX_DEFAULT_LANGUAGE','en-US');
 })();
