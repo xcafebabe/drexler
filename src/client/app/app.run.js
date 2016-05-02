61e73c2b1cc8ee33859331d9da3a57355f491d9d
Luis Toubes <luis@toub.es>
2016-05-03 01:32:58 +0200

    getPreferredLanguage from cordovaGlobalization is async. Reverted back the way how we set the language.

--- src/client/app/app.run.js
+++ src/client/app/app.run.js
@@ -11,21 +11,23 @@
       var languageSelected = drexlerStorage.get('language') || DRX_DEFAULT_LANGUAGE;
       if (angular.isObject(languageSelected)) { // Checking if local storage varibale for drexler exists
         log.debug('%s language from localstorage', languageSelected.id);
-        languageSelected = languageSelected.id;
+        gettextCatalog.setCurrentLanguage(languageSelected.id);
       } else { // If the local variable for language doesn't exists check for device language
         if ($ionicPlatform.is.name.length > 0) { // This is to just check for platform so that we don't get error on browser
           $cordovaGlobalization.getPreferredLanguage().then(
             function (deviceLang) { // Setting device language
               log.debug('%s language from device', deviceLang.value);
               languageSelected = deviceLang.value;
+              gettextCatalog.setCurrentLanguage(deviceLang.value);
             },
             function (error) { // If We can not get language from device either because shit happens sometimes we go for default
               log.error('Can not detect language in device, selecting default', error);
+              gettextCatalog.setCurrentLanguage(DRX_DEFAULT_LANGUAGE);
             });
+        }else {
+          gettextCatalog.setCurrentLanguage(DRX_DEFAULT_LANGUAGE);
         }
       }
-      gettextCatalog.setCurrentLanguage(languageSelected);
     });
-
   }
 })();
